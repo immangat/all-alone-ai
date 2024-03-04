5cb319c44745760283ed795ff898537035cc1cdd
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-03-04 08:58:35 -0800

    add new move and is valid functions

--- part_1/game_manager.py
+++ part_1/game_manager.py
@@ -15,6 +15,7 @@ class Manager:
         self.board = Board()
         self.board.setupBoard(setup)
         self.player1 = Player("Black")
+        # ???: What is the point of the flip cart
         self.player1.flipTurn()
         self.player2 = Player("White")
         self.displayBoard()
@@ -26,10 +27,39 @@ class Manager:
     def displayBoard(self):
         self.displayer.updateBoard(self.board)
 
-    #TODO Only works for single marbles and the top two rows have issues, need to fix
+    def _get_to_circle(self, selected_circle_row, selected_circle_number, direction):
+        print("selected_row", selected_circle_row)
+        print("selected_number", selected_circle_number)
+        to_circle = None
+        if direction == "R":
+            to_circle = (selected_circle_row, selected_circle_number + 1)
+        elif direction == "L":
+            to_circle = (selected_circle_row, selected_circle_number - 1)
+        elif direction == "UL":
+            to_circle = (chr(ord(selected_circle_row) + 1), selected_circle_number)
+        elif direction == "UR":
+            to_circle = (chr(ord(selected_circle_row) + 1), selected_circle_number + 1)
+        elif direction == "DL":
+            to_circle = (chr(ord(selected_circle_row) - 1), selected_circle_number - 1)
+        elif direction == "DR":
+            to_circle = (chr(ord(selected_circle_row) - 1), selected_circle_number )
+        return to_circle
+
+    def move_marble2(self, selected_circle, direction):
+        selected_circle_row, selected_circle_number = selected_circle
+        marble = self.board.getCircle(*selected_circle).getMarble()
+        to_circle = self._get_to_circle(selected_circle_row, selected_circle_number, direction)
+        print("circle", to_circle)
+        self.board.getCircle(*selected_circle).setMarble(None)
+        # Then, place the marble in the ending circle
+        self.board.getCircle(*to_circle).setMarble(marble)
+        # Update the display
+        self.displayBoard()
+
+    # TODO Only works for single marbles and the top two rows have issues, need to fix
     def moveMarble(self, selected_circles, to_circle):
         # Get the marble object from the starting circle
-        if isinstance(selected_circles, tuple): # handles the case when only one marble is selected
+        if isinstance(selected_circles, tuple):  # handles the case when only one marble is selected
             marble = self.board.getCircle(*selected_circles).getMarble()
             if self.isValidMove(selected_circles, to_circle, marble):
                 # If the move is valid, remove the marble from the starting circle
@@ -42,18 +72,27 @@ class Manager:
                 print("Invalid move")
 
         # else: # handles the case when multiple marbles are selected
-            # TODO This logic is currently not functional
-            # print("list of marbles selected")
-            # for marble in selected_circles:
-            #     if self.isValidMove(marble, to_circle, marble):
-            #         # If the move is valid, remove the marble from the starting circle
-            #         self.board.getCircle(*marble).setMarble(None)
-            #         # Then, place the marble in the ending circle
-            #         self.board.getCircle(*to_circle).setMarble(marble)
-            #         # Update the display
-            #         self.displayBoard()
-            #     else:
-            #         print("Invalid move")
+        # TODO This logic is currently not functional
+        # print("list of marbles selected")
+        # for marble in selected_circles:
+        #     if self.isValidMove(marble, to_circle, marble):
+        #         # If the move is valid, remove the marble from the starting circle
+        #         self.board.getCircle(*marble).setMarble(None)
+        #         # Then, place the marble in the ending circle
+        #         self.board.getCircle(*to_circle).setMarble(marble)
+        #         # Update the display
+        #         self.displayBoard()
+        #     else:
+        #         print("Invalid move")
+
+    def isValidMove2(self, selected_circle, direction):
+        selected_circle_row, selected_circle_number = selected_circle
+        to_circle = self._get_to_circle(selected_circle_row, selected_circle_number, direction)
+        print("to circle", to_circle)
+        to_circle_marble = self.board.getCircle(*to_circle).getMarble()
+        if to_circle_marble is None:
+            return True
+        return False
 
     def isValidMove(self, from_circle, to_circle, marble):
         # Check if the to_circle is one of the valid neighbors of from_circle
@@ -75,7 +114,6 @@ class Manager:
         # Switches the turn from one player to the other
         self.player1.flipTurn()
         self.player2.flipTurn()
-
 
 
 if __name__ == "__main__":
