3b4edf71bd3703d88ffd974ad795d103ff11e0c6
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-03-06 11:48:32 -0800

    add getter timer

--- part_1/timer.py
+++ part_1/timer.py
@@ -1,11 +1,14 @@
 class Timer:
     def __init__(self):
         self.minutes = 0
-        self.seconds = 15
+        self.seconds = 60
 
     def restart_timer(self):
-        self.seconds = 15
+        self.seconds = 60
 
     def increment_timer(self):
         self.seconds -= 1
+        return self.seconds
+
+    def get_time(self):
         return self.seconds
