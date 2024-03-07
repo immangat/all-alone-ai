e2f34840e877c4acc9d9c7db645b6fc5db5c2697
Tomasz Stojek <stojek604@gmail.com>
2024-03-07 05:31:06 -0800

    added comments and refactored names for move.py functions and their uses

--- part_1/move.py
+++ part_1/move.py
@@ -1,7 +1,19 @@
 class Move:
+    """
+    Class to represent a move in the game
+    """
     def __init__(self, circles, enum_direction):
+        """
+        Initialise the move object
+        :param circles: are the circles objects selected for the move as a list
+        :param enum_direction: is the direction of the move applied to each circle
+        """
         self.circles = circles  # list of tuples that represent the circles
         self.enum_direction = enum_direction # not value of direction but the enum of direction
 
     def __str__(self):
+        """
+        A string representation of the move object
+        :return: is the string representation of the move object
+        """
         return f"Move: {self.circles} {self.enum_direction.name}\t"
