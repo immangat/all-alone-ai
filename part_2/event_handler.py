812e0fa99b89bce069a6d511867d14b1ad0f0b78
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2024-03-11 20:25:24 -0700

    added highlighting board functionality

--- part_2/event_handler.py
+++ part_2/event_handler.py
@@ -10,6 +10,8 @@ class EventHandler:
             if event.type == pygame.QUIT:
                 pygame.quit()
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left click
+                self.game_window.draw_board()
+                self.game_window.updateWindow()
                 self.on_mouse_click(event.pos)
 
     def on_mouse_click(self, mouse_pos):
@@ -20,4 +22,18 @@ class EventHandler:
         for coord in self.game_window.get_board_tuples():
             marble_pos = self.game_window.board_to_pixel(coord)
             if self.game_window.is_within_circle(mouse_pos, marble_pos, self.game_window.marble_radius):
-                print(f"Marble at {coord} was clicked.")
+                if coord not in self.game_window.highlighted_marbles:
+                    print(f"Marble at {coord} was added.")
+                    self.highlight_circle(coord, self.game_window.marble_radius)
+                    self.game_window.highlighted_marbles.append(coord)
+                elif coord in self.game_window.highlighted_marbles:
+                    print(f"Marble at {coord} was removed.")
+                    self.game_window.highlighted_marbles.remove(coord)
+                    self.game_window.draw_board()
+                break
+
+    def highlight_circle(self, coord, radius):
+        # Draw a circle with a different color to highlight it
+        x_pixel, y_pixel = self.game_window.board_to_pixel(coord)
+        pygame.draw.circle(self.game_window.display_surface, (255, 102, 102), (x_pixel, y_pixel), radius + 3, 3)
+        pygame.display.flip()
