28369fec4f8fd0b2a8299719306095acf21360ca
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2024-03-15 11:30:31 -0700

    improves ui look of menu slightly

--- part_2/menu_screen.py
+++ part_2/menu_screen.py
@@ -2,9 +2,11 @@ import pygame
 import pygame_gui
 import pygame_menu
 from pygame_gui.elements import UIButton
+from functools import partial
 
 from part_2.board import Board
 from part_2.event_handler import EventHandler
+from part_2.player import HumanPlayer
 
 
 class MenuScreen:
@@ -20,8 +22,7 @@ class MenuScreen:
         self.manager = manager
         self.event_handler = EventHandler(self, manager)
         self.type = "menu"
-
-
+        self.human_player = HumanPlayer("Black", "b")
 
     def initWindow(self):
         pygame.init()
@@ -33,17 +34,34 @@ class MenuScreen:
         self.menu = pygame_menu.Menu('All-Alone', self.width, self.height,
                                      theme=pygame_menu.themes.THEME_DEFAULT)
 
-        self.menu.add.label("Game Settings:", padding=(0, 0, 15, 0)) # top, right, bottom, left
-        self.menu.add.button('Play against Human', self.play_against_human)
-        self.menu.add.button('Play against AI', self.play_against_ai)
-        self.menu.add.label("Marble Settings:")
-        self.menu.add.button('White Marble', self.select_white_marble)
-        self.menu.add.button('Black Marble', self.select_black_marble)
-        self.menu.add.button('Random Marble', self.select_random_marble)
-        self.menu.add.label("Select Board Type:", padding=(15, 0, 15, 0))
-        self.menu.add.selector('',
-                               [('Default', 1), ('German Daisy', 2), ('Belgian Daisy', 3)],
-                               onchange= self.select_board_type)
+        title = self.menu.add.label("Game Settings:", padding=(0, 0, 15, 0))  # top, right, bottom, left
+        selector_game_type = self.menu.add.dropselect(
+            title='Game Type:',
+            items=[('Human vs Human', 0),
+                   ('Human vs AI', 1),
+                   ('AI vs Human', 2)],
+            font_size=20,
+            selection_option_font_size=20,
+            onchange=self.select_game_type,
+            default=0,
+            selection_box_height=5,
+            selection_box_width=212,
+            selection_color=(76, 0, 153)
+        )
+
+        # board_type_frame = self.menu.add.frame_h(width=600, height=200, padding=(0, 50, 0, 0))
+        board_selector = self.menu.add.selector('Select Board Type:',
+                                                [('Default', 1), ('German Daisy', 2), ('Belgian Daisy', 3)],
+                                                onchange=self.select_board_type,
+                                                font_size=20,
+                                                style=pygame_menu.widgets.SELECTOR_STYLE_FANCY,
+                                                selection_color=(76, 0, 153))
+        # board_type_frame.pack(title_board, align=pygame_menu.locals.ALIGN_LEFT)
+        # board_type_frame.pack(board_selector, align=pygame_menu.locals.ALIGN_RIGHT)
+        # self.menu.add.label("Select Board Type:", padding=(15, 0, 15, 0))
+        # self.menu.add.selector('',
+        #                        [('Default', 1), ('German Daisy', 2), ('Belgian Daisy', 3)],
+        #                        onchange=self.select_board_type)
         self.menu.add.label("Time Settings:", padding=(15, 0, 15, 0))
         self.menu.add.button('Select Total Move Limit', self.select_total_move_limit)
         self.menu.add.text_input('Select P1 Time Per Move ',
@@ -53,7 +71,8 @@ class MenuScreen:
                                  input_type=pygame_menu.locals.INPUT_INT,
                                  cursor_selection_enable=False,
                                  textinput_id='timer_p1',
-                                 onchange=self.select_p1_time_per_move)
+                                 onchange=self.select_p1_time_per_move,
+                                 selection_color=(76, 0, 153))
         self.menu.add.text_input('Select P2 Time Per Move ',
                                  default='60',
                                  maxchar=3,
@@ -65,29 +84,28 @@ class MenuScreen:
         self.menu.add.button('Board Testing', self.select_total_move_limit)
 
         self.menu.add.button('Start', self.start_game)
-        self.menu.add.button('Quit', self.quit_game)
+        self.menu.add.button('Quit', self.quit_game, background_color=(205, 92, 92))
 
         self.manager.board.setup_board("Default")
 
+    def select_game_type(self, *args):
+        print(f"select game type {args[1]}")
+        if args[1] == 0:
+            self.human_vs_human()
+        elif args[1] == 1:
+            self.human_vs_ai()
+        elif args[1] == 2:
+            self.ai_vs_human()
 
+    def human_vs_human(self):
+        print("human vs human")
 
+    def human_vs_ai(self):
+        # self.add_marble_options()
+        print("human vs ai")
 
-
-
-    def play_against_human(self):
-        print("Play against human")
-
-    def play_against_ai(self):
-        print("Play against AI")
-
-    def select_white_marble(self):
-        print("White marble selected")
-
-    def select_black_marble(self):
-        print("Black marble selected")
-
-    def select_random_marble(self):
-        print("Random marble selected")
+    def ai_vs_human(self):
+        print("ai vs human")
 
     def select_board_type(self, selected: tuple, value: any):
         print("Board type selected")
@@ -121,7 +139,6 @@ class MenuScreen:
     def quit_game(self):
         # Exit the program
         pygame.quit()
-
 
     def updateWindow(self):
         # self.manager_ui.draw_ui(self.background)
