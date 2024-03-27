3fa275ade3c2e2b625dbedc549094992e52e6551
Vitor Guara <vrosaguara@my.bcit.ca>
2024-03-27 14:31:05 -0700

    Removed myTest from gitignore

--- part_3/myTest.py
+++ part_3/myTest.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     board.setup_board("Belgian Daisy")
     agent = AIAgent()
     start = time.process_time()
-    move = agent.get_best_move(board, "b", 5)
+    move = agent.get_best_move(board, "b", 4)
     print(f"b:{move[0]}")
     print(time.process_time() - start)
     # move = agent.get_best_move(move[1], "w", 3)
